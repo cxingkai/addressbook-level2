4c26f8ea43566a2c8900a2e4f7d33bef7219fb67
cxingkai <cxingkai@yahoo.com.sg>
2018-02-12 14:49:03 +0800

    Moved toString, equals, hashCode, isPrivate to Contact class

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -21,25 +21,4 @@ public class Address extends Contact{
         super(address, isPrivate);
         checkValid(ADDRESS_VALIDATION_REGEX, MESSAGE_ADDRESS_CONSTRAINTS);
     }
-
-    @Override
-    public String toString() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
-    }
-
-    @Override
-    public int hashCode() {
-        return value.hashCode();
-    }
-
-    public boolean isPrivate() {
-        return isPrivate;
-    }
 }
